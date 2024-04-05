52195e9dc57c5e1117df5b2ee6f44706aac457d5
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-04-04 21:51:38 -0300

    Base API

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,7 @@
-import { error } from "console";
 import { fastify } from "fastify";
 
-const app = fastify();
+const app = fastify()
 
-app.listen({ port: 3000 }, (erro, address) => {
-    if (erro) {
-    console.error(error)
-    process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-});
+app.listen({port: 3333}).then( () => {
+    console.log('SERVIDOR RODANDO')
+})
