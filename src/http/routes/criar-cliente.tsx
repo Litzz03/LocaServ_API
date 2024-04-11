384621126f023c23f5a1466dc2aab587b1c2d8c4
Douglas6969 <133924667+Douglas6969@users.noreply.github.com>
2024-04-11 00:48:45 -0300

    sadsa

--- src/http/routes/criar-cliente.tsx
+++ src/http/routes/criar-cliente.tsx
@@ -4,26 +4,25 @@ import { prisma } from "../../lib/prisma";
 
 export async function criarUsuario(app: FastifyInstance){
     app.post('/criarUsuario', async (request: FastifyRequest, reply: FastifyReply) => {
-       
+        try {
             const requestBody = z.object({
-                EMAIL_USUARIO: z.string(),
-                SENHA_USUARIO: z.string()
-
+                email: z.string(),
+                senha: z.string()
             });
     
-            const { EMAIL_USUARIO,SENHA_USUARIO } = requestBody.parse(request.body);
+            const { email, senha } = requestBody.parse(request.body);
         
             const usuarioCriado = await prisma.usuario.create({
                 data: {
-                
-                    EMAIL_USUARIO,
-                    SENHA_USUARIO
+                    EMAIL_USUARIO: email,
+                    SENHA_USUARIO: senha
                 }
             });
     
             return reply.status(201).send(usuarioCriado);
-        
+        } catch (error) {
+            console.error("Erro ao criar usuário:", error);
+            return reply.status(500).send({ error: "Erro ao criar usuário" });
+        }
     });
-
-
 }
