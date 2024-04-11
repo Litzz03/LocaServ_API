5c3f011725279886dc7ca949fde4ba6beb882c61
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-04-11 14:05:30 -0300

    Registro das rotas

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,29 @@
 import { fastify } from "fastify";
 import { criarUsuario } from "./routes/criar-usuario";
+import { consultarProduto } from "./routes/consulta-produtos";
+import { consultarCliente } from "./routes/consultar-clientes";
+import { consultarEmpresa } from "./routes/consultar-empresa";
+import { criarCliente } from "./routes/criar-cliente";
+import { criarEmpresa } from "./routes/criar-empresa";
+import { criarProduto } from "./routes/criar-produto";
+import { editarCliente } from "./routes/editar-cliente";
+import { editarEmpresa } from "./routes/editar-empresa";
+import { editarProduto } from "./routes/editar-produtos";
+import { excluirProduto } from "./routes/excluir-produtos";
 
 const app = fastify()
 
-app.register(criarUsuario)
+app.register(consultarProduto);
+app.register(consultarCliente);
+app.register(consultarEmpresa);
+app.register(criarCliente);
+app.register(criarEmpresa);
+app.register(criarProduto);
+app.register(criarUsuario);
+app.register(editarCliente);
+app.register(editarEmpresa);
+app.register(editarProduto);
+app.register(excluirProduto);
 
 app.listen({port: 3333}).then( () => {
     console.log('SERVIDOR RODANDO')
