b359645b6da65bd30244ba354dcb64902a1b8b7e
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-04-11 01:53:59 -0300

    Criação da rota de Criar Usuario

---
+++ src/http/routes/criar-usuario.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+
+export async function criarUsuario(app: FastifyInstance) {
+    
+    app.post('/criarUsuario', async (request, reply) => {
+    
+        const requestBody = z.object(
+            {
+                login_usuario : z.string  (),
+                senha_usuario : z.string  (),
+                email_usuario : z.string  ()
+            }
+        )
+
+        const { login_usuario, senha_usuario, email_usuario} = requestBody.parse(request.body);
+
+        const usuarioCriado = await prisma.usuario_sistema.create({
+            data: {
+                login_usuario,
+                senha_usuario,
+                email_usuario
+            }
+        })
+        return reply.status(201).send(usuarioCriado);
+    })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,9 @@
 import { fastify } from "fastify";
+import { criarUsuario } from "./routes/criar-usuario";
 
 const app = fastify()
+
+app.register(criarUsuario)
 
 app.listen({port: 3333}).then( () => {
     console.log('SERVIDOR RODANDO')
